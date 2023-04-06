ce348e27601e2b07a29c8d45a09edd72b90f07a3
Upal Bhattacharya <upal.bhattacharya@gmail.com>
2023-04-06 16:41:51 +0530

    #85zru315c - Updated paths for data to be correct on server.

--- DHC/common/fold_creation/run_create_splits_multilabel.sh
+++ DHC/common/fold_creation/run_create_splits_multilabel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-./create_splits_multilabel.py --input /home/workboots/Datasets/DHC/common_new/preprocess/fact_selected_cases.txt \
-    --targets /home/workboots/Datasets/DHC/common_new/targets/case_advs.json \
+./create_splits_multilabel.py --input /DATA/upal/Datasets/DHC/common_new/preprocess/fact_selected_cases.txt \
+    --targets /DATA/upal/Datasets/DHC/common_new/targets/case_advs.json \
     --folds 1 \
     --train_size 0.7 \
     --test_size 0.2 \

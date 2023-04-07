e4670b50be8907fa126ed2906c02f55f10245f67
Upal Bhattacharya <upal.bhattacharya@gmail.com>
2023-04-07 09:19:19 +0530

    #85zru315c - Fixed paths for selecting correcting files on server.

--- DHC/common/fold_creation/run_create_splits_multilabel.sh
+++ DHC/common/fold_creation/run_create_splits_multilabel.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-./create_splits_multilabel.py --input /DATA/upal/Datasets/DHC/common_new/preprocess/fact_selected_cases.txt \
-    --targets /DATA/upal/Datasets/DHC/common_new/targets/case_advs.json \
+./create_splits_multilabel.py --input /DATA/upal/Datasets/DHC/variations/new/var_6/adv_info/overall/selected_cases.txt \
+    --targets /DATA/upal/Datasets/DHC/variations/new/var_6/targets/case_advs.json \
     --folds 1 \
     --train_size 0.7 \
     --test_size 0.2 \
     --val_size 0.1 \
-    --output_path .
+    --output_path /DATA/upal/Datasets/DHC/variations/new/var_6/cross_val/1_fold/

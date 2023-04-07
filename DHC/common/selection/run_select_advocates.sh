eb7ae6a088ed5fe44b46388828da4ad9748e0e25
Upal Bhattacharya <upal.bhattacharya@gmail.com>
2023-04-07 09:39:23 +0530

    #85zru315c - Updated paths for selection of advocates.

--- DHC/common/selection/run_select_advocates.sh
+++ DHC/common/selection/run_select_advocates.sh
@@ -7,13 +7,13 @@ for fold in $(seq 0 $(($FOLDS - 1)))
 do
     for split in $SPLITS
     do
-        ./select_advocates.py -ac ~/Datasets/DHC/common_new/adv_info/adv_cases.json \
+        ./select_advocates.py -ac /DATA/upal/Datasets/DHC/common_new/adv_info/adv_cases.json \
             -n  0 \
-            -p ~/Datasets/DHC/variations/new/var_6/adv_info/overall/pet_cases.json \
-            -d ~/Datasets/DHC/variations/new/var_6/adv_info/overall/res_cases.json \
-            -o ~/Datasets/DHC/variations/new/var_6/adv_info/${FOLDS}_fold/fold_${fold}/${split}/ \
-            -s ~/Datasets/DHC/variations/new/var_6/cross_val/${FOLDS}_fold/fold_${fold}_${split}_cases.txt \
-            -sa ~/Datasets/DHC/variations/new/var_6/adv_info/overall/selected_advs.txt
+            -p /DATA/upal/Datasets/DHC/variations/new/var_6/adv_info/overall/pet_cases.json \
+            -d /DATA/upal/Datasets/DHC/variations/new/var_6/adv_info/overall/res_cases.json \
+            -o /DATA/upal/Datasets/DHC/variations/new/var_6/adv_info/${FOLDS}_fold/fold_${fold}/${split}/ \
+            -s /DATA/upal/Datasets/DHC/variations/new/var_6/cross_val/${FOLDS}_fold/fold_${fold}_${split}_cases.txt \
+            -sa /DATA/upal/Datasets/DHC/variations/new/var_6/adv_info/overall/selected_advs.txt
     done
 done
 

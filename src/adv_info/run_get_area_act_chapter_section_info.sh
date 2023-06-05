43168fa5c5fe51e86234b9dbdfc045afc05866cf
Upal Bhattacharya <upal.bhattacharya@gmail.com>
2023-06-05 13:23:24 +0530

    Update scripts for different runs

--- src/adv_info/run_get_area_act_chapter_section_info.sh
+++ src/adv_info/run_get_area_act_chapter_section_info.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-./get_area_act_chapter_section_info.py --input_path ~/Datasets/DHC/variations/v5/area_act_chapter_section_info/overall/case_area_act_chapter_section_info.json \
-    --case_advs ~/Datasets/DHC/variations/v5/targets/case_advs.json \
-    --selected_advs ~/Datasets/DHC/variations/v5/adv_info/overall/selected_advs.txt \
-    --output_path ~/Datasets/DHC/variations/v5/adv_info/overall/area_act_chapter_section_info
+./get_area_act_chapter_section_info.py --input_path ~/Datasets/DHC/common/area_act_chapter_section_info/case_area_act_chapter_section_info.json \
+    --case_advs ~/Datasets/DHC/common/targets/case_advs.json \
+    --selected_advs ~/Datasets/DHC/common/adv_info/all_advs.txt \
+    --output_path ~/Datasets/DHC/common/adv_info/area_act_chapter_section_info
 
--- src/targets/run_get_silver_targets.sh
+++ src/targets/run_get_silver_targets.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 ./get_silver_targets.py --data_targets ~/Datasets/DHC/variations/v5/targets/case_advs.json \
-  --attribute_targets ~/Datasets/DHC/variations/v5/adv_info/overall/area_act_chapter_section_info/section_adv_info.json \
+  --attribute_targets ~/Datasets/DHC/variations/v5/adv_info/overall/area_act_chapter_section_info/chapter_adv_info.json \
   --target_attribute_info ~/Datasets/DHC/variations/v5/adv_info/overall/area_act_chapter_section_info/adv_area_act_chapter_section_info.json \
   --data_attribute_info ~/Datasets/DHC/variations/v5/area_act_chapter_section_info/overall/case_area_act_chapter_section_info.json \
   --similarity 1.0 \
-  --key "sections" \
-  --omit_attribute ~/Datasets/DHC/variations/v5/area_act_chapter_section_info/sections_to_omit.txt \
+  --key "chapters" \
+  --omit_attribute ~/Datasets/DHC/variations/v5/area_act_chapter_section_info/chapters_to_omit.txt \
   --output_dir ~/Datasets/DHC/variations/v5/targets
